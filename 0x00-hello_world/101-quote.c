91d7d34287e68957266a0f475368cecc3e1e8d37
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-02-17 07:36:37 +0200

    Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
